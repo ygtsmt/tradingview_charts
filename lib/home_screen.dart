0f11e1f0d554a8c045de9874444407910e2f1c90
ygtsmt <ygtsmt@outlook.com>
2024-10-12 23:53:28 +0300

    minSdk verison update

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -16,6 +16,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: const Text('Flutter TradingView Chart'),
       ),
       body: const ChartWidget(
